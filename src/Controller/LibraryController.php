cbf67aca1a910486612049e88210d0658d3a7561
Racol <rcolboc.dev@gmail.com>
2021-03-15 16:24:19 +0100

    Not mandatory bonus but usefull for bonus 4 persist library

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Library;
 use App\Form\Type\LibraryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,13 +13,32 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class LibraryController extends AbstractController
 {
     /**
-     * @Route("/library/new", name="library_new")
+     * @Route("/library/add", name="library_add")
      * @param Request             $request
      * @param TranslatorInterface $translator
      */
     public function new(Request $request, TranslatorInterface $translator): Response
     {
-        $form = $this->createForm(LibraryType::class);
+        $user       = $this->getUser();
+        $em         = $this->getDoctrine()->getManager();
+        $library    = new Library();
+
+        $form = $this->createForm(LibraryType::class, $library);
+        $form->handleRequest($request);
+       
+        if ($form->isSubmitted() && $form->isValid()) {
+            $library    = $form->getData();
+
+            $user->addLibrary($library);
+
+            $em->persist($library);
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', $translator->trans('success.library'));
+
+            return $this->redirectToRoute('library_add');
+        }
 
         return $this->render('card/edit.html.twig', [
             'form'      => $form->createView(),
