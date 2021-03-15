0f270f0744a46bee72fa2963a54c3581944f067b
Racol <rcolboc.dev@gmail.com>
2021-03-15 15:37:43 +0100

    Bonus 2 multi users

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,15 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\Type\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * @Route("/register", name="app_register")
+     * @param Request                      $request
+     * @param TranslatorInterface          $translator
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function register(Request $request, TranslatorInterface $translator, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encode the new users password
+            $user->setPassword($passwordEncoder->encodePassword($user, $user->getPassword()));
+
+            // Set their role
+            $user->setRoles(['ROLE_USER']);
+
+            // Save
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', $translator->trans('success.account.created'));
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
