62e71c4a4efbd7968a357552df406aa4ac29b560
Racol <rcolboc.dev@gmail.com>
2021-03-15 18:03:01 +0100

    Update Version20210315114456.php

--- migrations/Version20210315114456.php
+++ migrations/Version20210315114456.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version202103151554316 extends AbstractMigration
+final class Version20210315114456 extends AbstractMigration
 {
     public function getDescription() : string
     {
